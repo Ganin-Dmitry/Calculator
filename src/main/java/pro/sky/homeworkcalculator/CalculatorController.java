e39d0406a807e87fdbdcd898f17f9b9797a88418
ganin <ganin.pro1995@gmail.com>
2024-08-12 21:51:29 +0300

    Сделано ДЗ

---
+++ src/main/java/pro/sky/homeworkcalculator/CalculatorController.java
@@ -0,0 +1,35 @@
+package pro.sky.homeworkcalculator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+    public CalculatorController(CalculatorService calculatorService){
+        this.calculatorService = calculatorService;
+    }
+    @GetMapping
+    public String hello () {
+        return calculatorService.hello();
+    }
+    @GetMapping(path = "/plus")
+    public String plus (@RequestParam(value = "num1", required = false) String oneValue, @RequestParam (value = "num2", required = false) String twoValue) {
+        return calculatorService.plus(oneValue, twoValue);
+    }
+    @GetMapping(path = "/minus")
+    public String minus (@RequestParam (value = "num1", required = false) String oneValue, @RequestParam (value = "num2", required = false) String twoValue) {
+        return calculatorService.minus(oneValue, twoValue);
+    }
+    @GetMapping(path = "/multiply")
+    public String multiply (@RequestParam (value = "num1", required = false) String oneValue, @RequestParam (value = "num2", required = false) String twoValue) {
+        return calculatorService.multiply(oneValue, twoValue);
+    }
+    @GetMapping(path = "/divide")
+    public String divide (@RequestParam (value = "num1", required = false) String oneValue, @RequestParam (value = "num2", required = false) String twoValue) {
+        return calculatorService.divide(oneValue, twoValue);
+    }
+}
---
+++ src/main/java/pro/sky/homeworkcalculator/CalculatorServiceImpl.java
@@ -0,0 +1,59 @@
+package pro.sky.homeworkcalculator;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    public String hello () {
+        return "Добро пожаловать в калькулятор!";
+    }
+
+    public String plus (String oneValue, String twoValue) {
+        if (!oneValue.isBlank() && !twoValue.isBlank()) {
+            int intOneValue = Integer.parseInt(oneValue);
+            int intTwoValue = Integer.parseInt(twoValue);
+            return oneValue + " + " + twoValue + " = " + Integer.toString(intOneValue + intTwoValue);
+        } else {
+            return valueError();
+        }
+    }
+    public String minus (String oneValue, String twoValue) {
+        if (!oneValue.isBlank() && !twoValue.isBlank()) {
+            int intOneValue = Integer.parseInt(oneValue);
+            int intTwoValue = Integer.parseInt(twoValue);
+            return oneValue + " - " + twoValue + " = " + Integer.toString(intOneValue - intTwoValue);
+        } else {
+            return valueError();
+        }
+    }
+    public String multiply (String oneValue,String twoValue) {
+        if (!oneValue.isBlank() && !twoValue.isBlank()) {
+            int intOneValue = Integer.parseInt(oneValue);
+            int intTwoValue = Integer.parseInt(twoValue);
+            return oneValue + " * " + twoValue + " = " + Integer.toString(intOneValue * intTwoValue);
+        } else {
+            return valueError();
+        }
+    }
+    public String divide (String oneValue, String twoValue) {
+        if (!oneValue.isBlank() && !twoValue.isBlank()) {
+            int intOneValue = Integer.parseInt(oneValue);
+            int intTwoValue = Integer.parseInt(twoValue);
+            if (intTwoValue == 0){
+                return nullError();
+            } else {
+                return oneValue + " / " + twoValue + " = " + Integer.toString(intOneValue / intTwoValue);
+            }
+        } else {
+            return valueError();
+        }
+
+    }
+    private String nullError() {
+        return "<b>ERROR: На ноль делить нельзя!</b>";
+    }
+    private String valueError() {
+        return "<b>ERROR: Не указано значение!</b>";
+    }
+}
